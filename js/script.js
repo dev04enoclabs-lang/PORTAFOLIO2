31dfd905b1bdf229b2d69fc35e4341a537ea0e89
yisus_07 <dev04enoclabs@gmail.com>
2026-03-19 15:55:01 -0600

    aprendiendo a usar el js como cambiar el tema del titulo

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
-document.getElementById("name");
+/*Se debe de encargar de cambiar el tema a el titulo*/
+const elTitulo =document.getElementById("elTitulo");
+const elBoton = document.getElementById("elBoton");
+const elBoton1 = document.getElementById("elBoton1");
 
+elBoton.addEventListener ("click", () => {
+    elTitulo.style.color = "red";
+});
+elBoton1.addEventListener ("click", () => {
+    elTitulo.style.color = "white";
+});
